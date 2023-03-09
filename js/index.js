fda9b38fcd582ede771eaf2c7ea892e35509e14e
lexa-makeev <makeevip@yandex.ru>
2023-03-09 22:35:13 +0300

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#button_work,#websiteHeader,#footer_nav").on("click", "a", function (event) {
+    $("#banner_hd").on("click", "a", function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
