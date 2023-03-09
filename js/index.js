5d01be82c5aaed13a9fb8e9b1a908565a80897db
IvankaIvanov97 <zorkin1923@gmail.com>
2023-03-09 03:07:07 +0300

    IT's ALL

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#button_work,#websiteHeader,#footer_nav").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
+    $(document).on("click", "button", function (event) {
+        $( ".backg_cts" ).toggleClass( "enable" )
+    });
+    $(document).on("click", ".wind_cts img", function (event) {
+        $( ".backg_cts" ).toggleClass( "enable" )
+    });
+    
+});
+
+function gam_click() {
+    $(".gam").css("top", "0");
+    $("#websiteHeader").css("display", "none");
+}
+
+function close_gam() {
+    $(".gam").css("top", "-4000px");
+    $("#websiteHeader").css("display", "block");
+}
